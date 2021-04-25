94676d388d27d7ef55240bccf076fc215b0c6a61
Onunez-g <nunez.orlando2@gmail.com>
2021-04-25 16:06:02 -0400

    Load data

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 
+	"github.com/onunez-g/jsonresume-api/models"
 	"github.com/onunez-g/jsonresume-api/routes"
 )
 
 func main() {
+	loadData()
 	r := routes.GetRoutes()
 	log.Fatal(r.Run())
+}
+
+func loadData() {
+	f, err := ioutil.ReadFile("./data.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	err = json.Unmarshal(f, &models.MyResume)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
